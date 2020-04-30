b4fd7d155ab8c3e0fa03e129c0145b5691f9c2d2
enesoral <oralenes0@gmail.com>
2020-04-30 14:36:16 +0300

    Initial commit

---
+++ src/main/java/com/enesoral/mvcrest/MvcRestApplication.java
@@ -0,0 +1,13 @@
+package com.enesoral.mvcrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcRestApplication.class, args);
+	}
+
+}
